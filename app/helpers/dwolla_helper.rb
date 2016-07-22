e641936a4238520174f71260a0b25ca803364a27
Gordon Weeks <gchrisweeks@gmail.com>
2016-07-22 16:50:27 -0700

    Partial implementation of Dwolla

--- app/helpers/dwolla_helper.rb
+++ app/helpers/dwolla_helper.rb
@@ -2,62 +2,80 @@ module DwollaHelper
   require 'dwolla_v2'
 
   $dwolla = DwollaV2::Client.new(
-    id: 'XIvRyGL2IFvDNc0tGukSoTZ7yW9dCaZm6hwjzxaU2p5QhbWKTC',
-    secret: 'iscddv6GIjS4S3iKpLxFQD5002hkN4GheNGE2GE3JQnbZOOTSX') do |config|
+    id: ENV['DWOLLA_CLIENT_ID'],
+    secret: ENV['DWOLLA_CLIENT_SECRET']) do |config|
 
     config.environment = :sandbox
+    # TODO
     # config.environment = :production
   end
 
   @@account_token = $dwolla.tokens.new(
-    access_token: 'CekeHk4k3ZTcIxyfUpFhncAqGtrz1RVThB92Nmrhja3Ms8XoYg',
-    refresh_token: 'X0ZOfXjHv7wpLfwBc3czViQqhTgHxkWJK7tGjvarhhmZj570cS',
-    account_id: '8626764c-406d-4b40-aedb-4d70db59957a')
+    access_token: ENV['DWOLLA_ACCOUNT_ACCESS_TOKEN'],
+    refresh_token: ENV['DWOLLA_ACCOUNT_REFRESH_TOKEN'],
+    account_id: ENV['DWOLLA_ACCOUNT_ID']
+  )
 
-  def self.add_customer(user)
-    # jenny = DwollaSwagger::CreateCustomer.new
-    # jenny.first_name = user.fname
-    # jenny.last_name = user.lname
-    # jenny.email = user.email
-    # begin
-    #   location = DwollaSwagger::CustomersApi.create(body: jenny)
-    # rescue DwollaSwagger::ClientError => _e
-    #   # p eval(e.message)
-    #   return { error: 400 }
-    # rescue DwollaSwagger::ServerError => _e
-    #   # p eval(e.message)
-    #   return { error: 500 }
-    # end
-    # { location: location }
+  def self.dev_get_customers
+    ret = self.get('customers')
+    ret['_embedded']['customers']
   end
 
-  def self.get_funding_source(id)
-    funding_sources = @@account_token.get('https://api-uat.dwolla.com/customers/' + id + '/funding-sources')
-    # funding_sources._embedded['funding-sources'][0].name # => "Vera Brittain’s Checking"
-    return funding_sources
+  def self.dev_customer_exists(email)
+    ret = self.get('customers?search=' + email)
+    ret['_embedded']['customers'].size > 0 ? 'Yes' : 'No'
   end
 
-  def self.add_funding_source(customer_id)
-    request_body = {
-      routingNumber: '222222226',
-      accountNumber: '123456789',
-      type: 'checking',
-      name: 'Vera Brittain’s Checking'
-    }
+  def self.add_customer(user, ssn, ip)
+    # Format phone number for Dwolla
+    phone = user.number
+    phone.slice! '+1' if phone.length == 12
+    self.post('customers', {
+      firstName: user.fname,
+      lastName: user.lname,
+      email: user.email,
+      phone: phone,
+      ipAddress: ip,
+      type: 'personal',
+      address1: user.address.line1,
+      address2: user.address.line2,
+      city: user.address.city,
+      state: user.address.state,
+      postalCode: user.address.zip,
+      dateOfBirth: user.dob.to_s,
+      ssn: ssn
+    }, {
+      'Idempotency-Key': user.id.to_s
+    })
+  end
 
-    funding_source = @@account_token.post('https://api-uat.dwolla.com/customers/' + customer_id + '/funding-sources', request_body)
-    # funding_source.headers[:location] # => "https://api-uat.dwolla.com/funding-sources/375c6781-2a17-476c-84f7-db7d2f6ffb31"
-    return funding_source
+  def self.get_funding_source(user)
+    self.get('customers/' + user.dwolla_id + '/funding-sources')
+  end
+
+  def self.add_funding_source(user, account)
+    self.post('customers/' + user.dwolla_id + '/funding-sources', {
+      routingNumber: account.routing_num,
+      accountNumber: account.account_num,
+      type: 'checking', # TODO
+      name: account.name
+    }, {
+      'Idempotency-Key': user.dwolla_id + account.account_num
+    })
   end
 
   def self.transfer_money(customer_id, funding_source, amount)
-    request_body = {
+    self.post("transfers", {
       _links: {
         destination: {
           href: 'https://api-uat.dwolla.com/customers/' + customer_id
+          # TODO
+          #href: 'https://api.dwolla.com/customers/' + customer_id
         },
         source: {
           href: 'https://api-uat.dwolla.com/funding-sources/' + funding_source
+          # TODO
+          #href: 'https://api.dwolla.com/funding-sources/' + funding_source
         }
       },
       amount: {
@@ -68,9 +86,33 @@ module DwollaHelper
         foo: 'bar',
         baz: 'boo'
       }
-    }
+    }#, {
+    #  'Idempotency-Key': user.id.to_s
+    #}
+    )
+  end
 
-    transfer = @@account_token.post "transfers", request_body
-    return transfer.headers[:location] # => "https://api.dwolla.com/transfers/74c9129b-d14a-e511-80da-0aa34a9b2388"
+  private
+
+  def self.get(route)
+    begin
+      ret = @@account_token.get route
+    rescue DwollaV2::NotFoundError => e
+      return "ErrorNotFound"
+    rescue DwollaV2::Error => e
+      return e
+    end
+    ret
+  end
+
+  def self.post(route, payload, headers)
+    begin
+      ret = @@account_token.post route, payload, headers
+    rescue DwollaV2::NotFoundError => e
+      return "ErrorNotFound"
+    rescue DwollaV2::Error => e
+      return e
+    end
+    ret
   end
 end
