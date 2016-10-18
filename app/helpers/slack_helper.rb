214fe21efb6f3ec287521836d8c30ad1bf1e393d
Gordon Weeks <gchrisweeks@gmail.com>
2016-10-17 17:25:55 -0700

    Disable Slack notifications for anything except production

--- app/helpers/slack_helper.rb
+++ app/helpers/slack_helper.rb
@@ -2,6 +2,7 @@ module SlackHelper
   def self.log(body)
     route = ENV['SLACK_EXCEPTIONS_ROUTE']
     return false if route.blank?
+    return false unless Rails.env.production?
     notifier = Slack::Notifier.new("https://hooks.slack.com/services/" + route)
     notifier.ping body
     true
