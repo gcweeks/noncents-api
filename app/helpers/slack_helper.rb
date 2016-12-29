49420a7757e57f4e59262aca11d2fc31ffe89a5c
Gordon Weeks <gchrisweeks@gmail.com>
2016-12-29 12:44:25 -0800

    Restored Slack logging for all production servers

--- app/helpers/slack_helper.rb
+++ app/helpers/slack_helper.rb
@@ -2,8 +2,6 @@ module SlackHelper
   def self.log(body)
     route = ENV['SLACK_EXCEPTIONS_ROUTE']
     return false if route.blank?
-    domain = ENV['DOMAIN']
-    return false unless domain == 'api.noncents.co'
     return false unless Rails.env.production?
     notifier = Slack::Notifier.new("https://hooks.slack.com/services/" + route)
     notifier.ping body
